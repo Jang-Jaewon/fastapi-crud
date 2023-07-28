cdea07846250b0b624becedb925d94841fd820dd
jewon119 <jewon119@gmail.com>
2023-07-28 13:03:09 +0900

    formatting 수정 및 isort 추가

--- migrations/env.py
+++ migrations/env.py
@@ -1,12 +1,10 @@
+import os
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-
-import os, sys
 from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
@@ -76,9 +74,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
