3816aae7008186dcc0d1d470f6e14ad403dde57b
jewon119 <jewon119@gmail.com>
2023-08-03 11:52:07 +0900

    Add: fastapi start

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from typing import List, Optional
 
-app = FastAPI()
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="Fast API CRUD",
+    description="FastAPI CRUD Study",
+    version="0.0.1",
+    contact={
+        "name": "Jaewon",
+        "email": "jewon119@google.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
+
+users = []
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str]
+
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return "Success"
+
+
+@app.get("/users/{id}")
+async def get_user(
+    id: int = Path(..., description="The ID of the user you want to retrieve.", gt=2),
+    q: str = Query(None, max_length=5),
+):
+    return {"user": users[id], "query": q}
