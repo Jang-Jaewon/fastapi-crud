a86fc2afb1634862287b8dcd29c040f7d0e189bc
jewon119 <jewon119@gmail.com>
2023-08-07 11:48:35 +0900

    Add: get user by id api 생성

--- api/users.py
+++ api/users.py
@@ -20,3 +20,8 @@ async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 @router.post("/users")
 async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db=db, user=user)
+
+
+@router.get("/users/{user_id}")
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    return get_user(db=db, user_id=user_id)
