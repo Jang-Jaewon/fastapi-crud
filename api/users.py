73bf387468b45af09f87924dce5e005868eed6f6
jewon119 <jewon119@gmail.com>
2023-08-07 11:31:14 +0900

    read users api router 설정

--- api/users.py
+++ api/users.py
@@ -1,30 +1,18 @@
-from typing import List, Optional
+from typing import List
 
 import fastapi
-from pydantic import BaseModel
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from api.utils.users import create_user, get_user, get_user_by_email, get_users
+from db.db_setup import get_db
+from schemas.user import User, UserCreate
 
 router = fastapi.APIRouter()
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
 
 @router.get("/users", response_model=List[User])
-async def get_users():
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
     return users
 
-
-@router.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return "Success"
-
-
-@router.get("/users/{id}")
-async def get_user(id: int):
-    return {"user": users[id]}
