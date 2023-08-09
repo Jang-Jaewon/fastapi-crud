d21401976aaa09f730ad70a71e5b157ca2bfeee3
jewon119 <jewon119@gmail.com>
2023-08-09 10:52:14 +0900

    Add: Course create, Course get_id api 추가

--- api/courses.py
+++ api/courses.py
@@ -15,3 +15,17 @@ router = fastapi.APIRouter()
 async def read_courses(db: Session = Depends(get_db)):
     courses = get_courses(db=db)
     return courses
+
+
+@router.post("/courses", response_model=Course)
+async def create_new_course(course: CourseCreate, db: Session = Depends(get_db)):
+    return create_course(db=db, course=course)
+
+
+@router.get("/courses/{course_id}")
+async def read_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = get_course(db=db, course_id=course_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return db_course
+
