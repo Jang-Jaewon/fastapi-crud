629710a85c4d61da1752f5588c7b100205761d89
jewon119 <jewon119@gmail.com>
2023-08-21 22:49:59 +0900

    Add: path parameter test

--- main.py
+++ main.py
@@ -21,3 +21,28 @@ async def put():
 @app.get("/deprecated", description="deprecated test", deprecated=True)
 async def get_deprecated():
     return {"message": "hello from the put route"}
+
+
+@app.get("/items")
+async def list_items():
+    return {"message": "list items route"}
+
+
+@app.get("/items/{items_id}")
+async def get_item(items_id: int):
+    return {"items_id": items_id}
+
+
+@app.get("/users")
+async def list_users():
+    return {"message": "list items route"}
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"message": "this is the current user"}
