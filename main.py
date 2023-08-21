1ace0e5d65285d0297cd1f5077bd44db4655da3d
jewon119 <jewon119@gmail.com>
2023-08-21 23:00:29 +0900

    Add: Enum test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -46,3 +47,20 @@ async def get_user(user_id: str):
 @app.get("/users/me")
 async def get_current_user():
     return {"message": "this is the current user"}
+
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {"food_name": food_name, "message": "you are still healthy, but like sweet things"}
+
+    return {"food_name": food_name, "message": "i like chocolate milk"}
