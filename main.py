518259bc075320c918a736998c204f0925e4cce3
jewon119 <jewon119@gmail.com>
2023-08-22 22:34:48 +0900

    Add: qeury parameter test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -24,14 +25,14 @@ async def get_deprecated():
     return {"message": "hello from the put route"}
 
 
-@app.get("/items")
-async def list_items():
-    return {"message": "list items route"}
+# @app.get("/items")
+# async def list_items():
+#     return {"message": "list items route"}
 
 
-@app.get("/items/{items_id}")
-async def get_item(items_id: int):
-    return {"items_id": items_id}
+# @app.get("/items/{items_id}")
+# async def get_item(items_id: int):
+#     return {"items_id": items_id}
 
 
 @app.get("/users")
@@ -64,3 +65,21 @@ async def get_food(food_name: FoodEnum):
         return {"food_name": food_name, "message": "you are still healthy, but like sweet things"}
 
     return {"food_name": food_name, "message": "i like chocolate milk"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip+limit]
+
+
+@app.get("/items/{item_id}")
+async def list_items(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat."})
+    return item
