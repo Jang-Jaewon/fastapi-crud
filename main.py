36abcd0f26d008243367ef47f3b8a0744be63211
jewon119 <jewon119@gmail.com>
2023-08-22 22:41:40 +0900

    Fix: 오타 수정 및 query param test2

--- main.py
+++ main.py
@@ -75,9 +75,29 @@ async def list_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip+limit]
 
 
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update({"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat."})
+#     return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat."})
+    return item
+
+
 @app.get("/items/{item_id}")
-async def list_items(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
+async def get_item(item_id: str, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
     if q:
         item.update({"q": q})
     if not short:
