d02976d15a7924689897a80a086a328149b7d211
JANG-JAEWON <jewon119@gmail.com>
2023-08-30 08:55:09 +0900

    Add: nested pydantic models and using url pattern

--- schema.py
+++ schema.py
@@ -3,12 +3,18 @@ from typing import Optional, List
 from pydantic import BaseModel, Field
 
 
+class Image(BaseModel):
+    url: str = Field(..., pattern="^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)$")
+    name: str
+
+
 class Item(BaseModel):
     name: str
     description: str | None = Field(None, title="The description of the item", max_length=100)
     price: float = Field(..., gt=0, description="The price must be greater than zero.")
     tax: float | None = None
-    tags: List[int] = []
+    tags: set[str] = set()
+    image: Image | None = None
 
 
 class User(BaseModel):
