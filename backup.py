bd4adc25a67ef4711c5b181f15ca5a7dcfc236e7
JANG-JAEWON <jewon119@gmail.com>
2023-09-05 08:12:47 +0900

    Add: Cookie and Header Parameters

--- backup.py
+++ backup.py
@@ -260,3 +260,23 @@ async def update_item(
 ):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.put("/items/{item_id}")
+async def read_items(
+    item_id: UUID,
+    start_date: datetime | None = Body(None),
+    end_date: datetime | None = Body(None),
+    repeat_at: time | None = Body(None),
+    process_after: timedelta | None = Body(None),
+):
+    start_process = start_date + process_after
+    duration = end_date - start_process
+    return {
+        "item_id:": item_id,
+        "start_date": start_date,
+        "end_date": end_date,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
+    }
--- main.py
+++ main.py
@@ -3,28 +3,23 @@ from enum import Enum
 from typing import Annotated, List, Optional
 from uuid import UUID
 
-from fastapi import Body, FastAPI, Path, Query
+from fastapi import Body, Cookie, FastAPI, Header, Path, Query
 
 from schema import Image, Importance, Item, Offer, User
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
+@app.get("/items")
 async def read_items(
-    item_id: UUID,
-    start_date: datetime | None = Body(None),
-    end_date: datetime | None = Body(None),
-    repeat_at: time | None = Body(None),
-    process_after: timedelta | None = Body(None),
+    cookie_id: str | None = Cookie(None),
+    accept_encoding: str | None = Header(None),
+    user_agent: str | None = Header(None),
+    x_token: list[str] | None = Header(None),
 ):
-    start_process = start_date + process_after
-    duration = end_date - start_process
     return {
-        "item_id:": item_id,
-        "start_date": start_date,
-        "end_date": end_date,
-        "repeat_at": repeat_at,
-        "start_process": start_process,
-        "duration": duration,
+        "cookie_id:": cookie_id,
+        "Accept_Encoding": accept_encoding,
+        "User_Agent": user_agent,
+        "X_Token": x_token,
     }
