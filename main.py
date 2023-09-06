3c0aa62b3f135fd730573a636edb7a9e0e0772a3
JANG-JAEWON <jewon119@gmail.com>
2023-09-07 08:31:01 +0900

    Add: Using HTTP status code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from enum import Enum
 from typing import Annotated, List, Literal, Optional, Union
 from uuid import UUID
 
-from fastapi import Body, Cookie, FastAPI, Header, Path, Query
+from fastapi import Body, Cookie, FastAPI, Header, Path, Query, status
 
 from schema import (Image, Importance, Item, Offer, User, UserBase, UserIn, PlaneItem, CarItem,
                     UserInDB, UserOut, ListItem)
@@ -11,17 +11,17 @@ from schema import (Image, Importance, Item, Offer, User, UserBase, UserIn, Plan
 app = FastAPI()
 
 
-@app.post("/items", status_code=201)
+@app.post("/items", status_code=status.HTTP_201_CREATED)
 async def crate_item(name: str):
     return {"name": name}
 
 
-@app.delete("/items/{[pk}", status_code=204)
+@app.delete("/items/{[pk}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_item(pk: str):
     print("pk", pk)
     return
 
 
-@app.get("/items", status_code=401)
+@app.get("/items", status_code=status.HTTP_302_FOUND)
 async def read_items_redirect():
     return {"hello": "world"}
