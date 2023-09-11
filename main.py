dac34a0f3b7b29b7274a68df7efa52b7e8005917
JANG-JAEWON <jewon119@gmail.com>
2023-09-12 08:36:16 +0900

    add: ResquestValidationError, HttpException in starlette

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from uuid import UUID
 
 from fastapi import (Body, Cookie, FastAPI, File, Form, Header, HTTPException,
                      Path, Query, Request, UploadFile, status)
-from fastapi.responses import JSONResponse
-
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, PlainTextResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from schema import (CarItem, Image, Importance, Item, ListItem, Offer,
                     PlaneItem, User, UserBase, UserIn, UserInDB, UserOut)
 
@@ -45,3 +46,20 @@ async def read_unicorns(name: str):
     if name == "yolo":
         raise UnicornException(name=name)
     return {"unicorn_name": name}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.get("/validation_items/{item_id}")
+async def read_validation_items(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't lik 3.")
+    return {"item_id": item_id}
