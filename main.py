7bf401df245ec0c5394659072b3999cbe2474c86
JANG-JAEWON <jewon119@gmail.com>
2023-09-12 08:25:01 +0900

    add: unicorn exception으로 exception 확장

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from typing import Annotated, List, Literal, Optional, Union
 from uuid import UUID
 
 from fastapi import (Body, Cookie, FastAPI, File, Form, Header, HTTPException,
-                     Path, Query, UploadFile, status)
+                     Path, Query, Request, UploadFile, status)
+from fastapi.responses import JSONResponse
 
 from schema import (CarItem, Image, Importance, Item, ListItem, Offer,
                     PlaneItem, User, UserBase, UserIn, UserInDB, UserOut)
@@ -24,3 +25,23 @@ async def read_item(item_id: str):
             headers={"X-Error": "Ther goes my error"},
         )
     return {"item": items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. there goes a rainbow."},
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorns(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
