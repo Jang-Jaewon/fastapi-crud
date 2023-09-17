6e451eb6395fcb45c5d0dcfc503f5855dbbdfd0b
JANG-JAEWON <jewon119@gmail.com>
2023-09-18 08:37:51 +0900

    add: Enum을 활용하여 tags 지정

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from uuid import UUID
 from fastapi import (Body, Cookie, FastAPI, File, Form, Header, HTTPException,
                      Path, Query, Request, UploadFile, status)
 from fastapi.encoders import jsonable_encoder
+from fastapi.exception_handlers import (http_exception_handler,
+                                        request_validation_exception_handler)
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse, PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
 
 from schema import (CarItem, Image, Importance, Item, ListItem, Offer,
                     PlaneItem, User, UserBase, UserIn, UserInDB, UserOut)
@@ -17,16 +18,28 @@ from schema import (CarItem, Image, Importance, Item, ListItem, Offer,
 app = FastAPI()
 
 
-@app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED, tags=["items"])
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
+
+@app.post(
+    "/items",
+    response_model=Item,
+    status_code=status.HTTP_201_CREATED,
+    tags=[Tags.items],
+    summary="Create an Item",
+    description="Create an item with all the information: name; description; price; tax; and a set of",
+)
 async def create_item(item: Item):
     return item
 
 
-@app.get("/items", tags=["items"])
+@app.get("/items", tags=[Tags.items])
 async def read_items():
     return [{"name": "Foo", "price": 42}]
 
 
-@app.get("/users", tags=["users"])
+@app.get("/users", tags=[Tags.users])
 async def read_users():
     return [{"username": "PhoebeBuffay"}]
