6908a15506a161e386f3f158042322d328ad62c3
JANG-JAEWON <jewon119@gmail.com>
2023-09-20 08:12:46 +0900

    fix

--- backup.py
+++ backup.py
@@ -539,3 +539,36 @@ async def read_users():
 @app.get("/elements", tags=[Tags.items], deprecated=True)
 async def read_elements():
     return [{"item_id": "Foo"}]
+
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items.get(item_id)
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+@app.patch("/items/{item_id}", response_model=Item)
+def patch_item(item_id: str, item: Item):
+    stored_item_data = items.get(item_id)
+    if stored_item_data is not None:
+        stored_item_model = Item(**stored_item_data)
+    else:
+        stored_item_model = Item()
+    update_data = item.model_dump(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
