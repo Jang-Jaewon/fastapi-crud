de30ea898424ee3dd904d20fc5ca7bf7550b638d
JANG-JAEWON <jewon119@gmail.com>
2023-09-20 08:18:42 +0900

    add: using Depends

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Annotated, List, Literal, Optional, Union
 from uuid import UUID
 
 from fastapi import (Body, Cookie, FastAPI, File, Form, Header, HTTPException,
-                     Path, Query, Request, UploadFile, status)
+                     Path, Query, Request, UploadFile, status, Depends)
 from fastapi.encoders import jsonable_encoder
 from fastapi.exception_handlers import (http_exception_handler,
                                         request_validation_exception_handler)
@@ -18,11 +18,18 @@ from schema import (CarItem, Image, Importance, Item, ListItem, Offer,
 app = FastAPI()
 
 
+async def hello():
+    return "world"
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)):
+    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+
 @app.get("/items")
-async def read_items(q: str | None = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
 
 
 @app.get("/users")
-async def read_users(q: str | None = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
