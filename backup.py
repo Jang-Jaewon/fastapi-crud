9140bdb74c21a7b04796b859096aa63c9d08585c
JANG-JAEWON <jewon119@gmail.com>
2023-09-25 08:37:57 +0900

    add: using dependencies

--- backup.py
+++ backup.py
@@ -610,3 +610,19 @@ async def read_items(commons: CommonQueryParams = Depends()):
     items = fake_items_db[commons.skip : commons.skip + commons.limit]
     response.update({"items": items})
     return response
+
+
+# Sub-Dependencies
+def query_extractor(q: str | None = None):
+    return q
+
+
+def query_or_body_extractor(q: str = Depends(query_extractor), last_query: str | None = Body(None)):
+    if q:
+        return q
+    return last_query
+
+
+@app.post("/item")
+async def try_query(query_or_body: str = Depends(query_or_body_extractor)):
+    return {"q_or_body": query_or_body}
