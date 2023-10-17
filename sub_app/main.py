fbf5f59afc5c342bc55f89cb75464e6c2b0b0c66
JANG-JAEWON <jewon119@gmail.com>
2023-10-18 08:24:31 +0900

    router 등록 방법2

--- sub_app/main.py
+++ sub_app/main.py
@@ -1,11 +1,13 @@
 from fastapi import Depends, FastAPI
 
 from .dependencies import get_query_token, get_token_header
-from .routers import items, users
+from .routers.users import router as user_router
+from .routers.items import router as item_router
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
-app.include_router(users.router)
-app.include_router(items.router)
+app.include_router(user_router)
+app.include_router(item_router)
+
 
 
 @app.get("/")
