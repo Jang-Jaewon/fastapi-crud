b7eaffe598547b5c4eb7cf8027a973c9d4463d4e
JANG-JAEWON <jewon119@gmail.com>
2023-10-20 08:51:14 +0900

    add: BackgroundTask add Dependences

--- sql_app/main.py
+++ sql_app/main.py
@@ -55,17 +55,33 @@ def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return items
 
 
+# BackgroundTasks
 import time
 
 
-def write_notification(email: str, message=""):
-    with open("log.txt", mode="w") as email_file:
-        content = f"notification for {email}: {message}"
-        time.sleep(5)
-        email_file.write(content)
+# def write_notification(email: str, message=""):
+#     with open("log.txt", mode="w") as email_file:
+#         content = f"notification for {email}: {message}"
+#         time.sleep(5)
+#         email_file.write(content)
+
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
+def get_query(background_task: BackgroundTasks, q: str):
+    if q:
+        message = f"found query {q}\n"
+        background_task.add_task(write_log, message)
+    return q
 
 
 @app.post("/end-notification/{email}", status_code=202)
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(write_notification, email, message="some notification")
-    return {"message": "Notification sent in the background"}
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent", "query": q}
