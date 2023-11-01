be92baa044b18c1109bb62d59e7ff0d603d805d2
JANG-JAEWON <jewon119@gmail.com>
2023-11-02 08:40:44 +0900

    add: test code 추가

--- test_main.py
+++ test_main.py
@@ -13,3 +13,59 @@ def test_read_item():
         "title": "Foo",
         "description": "There goes my here",
     }
+
+
+def test_read_item_bad_token():
+    response = client.get("/items/foo", headers={"X-Token":"wrong"})
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Invalid credentials X-Token headers"}
+
+
+def test_read_inexistent_item():
+    response = client.get("/items/baz", headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Item not found"}
+
+
+def test_create_item():
+    response = client.post(
+        "/items",
+        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Bartenders"},
+        headers={"X-Token": "coneofsilence"}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Bartenders",
+    }
+
+
+def test_create_item_bad_token():
+    response = client.post(
+        "/items",
+        headers={"X-Token": "badheader"},
+        json={
+            "id": "bazz",
+            "title": "Bazz",
+            "description": "Drop the bazz",
+        })
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Invalid X-Token header"
+    }
+
+
+def test_create_existing_item():
+    response = client.post(
+        "/items",
+        headers={"X-Token": "coneofsilence"},
+        json={
+            "id": "foo",
+            "title": "Bazz",
+            "description": "Drop the bazz",
+        })
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Item already exists"
+    }
